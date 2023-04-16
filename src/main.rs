f57a4018d496e8a1a044e92297696e71715fe8d3
Patrice Freydiere <frett27@gmail.com>
2023-04-16 18:24:27 +0200

    typo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-
-use log::{info};
+use log::info;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -15,12 +14,11 @@ fn main() {
     let mut native_options = eframe::NativeOptions::default();
     native_options.vsync = false;
     eframe::run_native(
-        "encrypter",
+        "OR1 Module File Encrypter",
         native_options,
         Box::new(|cc| Box::new(encrypter::EncrypterApp::new(cc))),
     );
 }
-
 
 // when compiling to web using trunk.
 #[cfg(target_arch = "wasm32")]
@@ -30,7 +28,6 @@ fn main() {
 
     // Redirect tracing to console.log and friends:
     tracing_wasm::set_as_global_default();
-
 
     let web_options = eframe::WebOptions::default();
 
