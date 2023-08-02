d712dfbd5f44eed135fde5c71db09be0a196ffdf
Patrice Freydiere <frett27@gmail.com>
2023-08-02 14:36:48 +0200

    fix file reading

--- tests/test_encryption.rs
+++ tests/test_encryption.rs
@@ -5,33 +5,37 @@ mod test_encryption {
 
     use encrypter::encrypt::*;
 
-    #[test]
-    fn test_encrypt() {
-        let midi_file: String = "lalala1.mid".into();
+   
+    fn encrypt_decrypt(midi_file: String) {
+        
         println!("encrypt midi file {}", &midi_file);
         encrypt_file(&midi_file.clone(), &"test_public.key.pem".into()).expect("fail to encrypt");
 
         decrypt_file(
-            midi_file + "x".into(),
+            midi_file.clone() + "x".into(),
             "test_private.key.pem".into(),
             "30d9690cc085429a1d0a3ae787932bf1518a1798".into(),
             "result".into(),
         )
         .expect("fail to decrypt");
-    }
 
+        // check result
+        let src = get_file_as_byte_vec(&midi_file).expect("cannot read the source file");
+        let dest = get_file_as_byte_vec(&"result".into()).expect("cannot read the destination file");
+        assert!(src.len() == dest.len());
+        for (index,i) in src.iter().enumerate() {
+            assert!(*i == dest[index]);
+        }
+
+    }
     #[test]
-    fn test_encrypt2() {
-        let midi_file: String = "t".into();
-        println!("encrypt midi file {}", &midi_file);
-        encrypt_file(&midi_file.clone(), &"test_public.key.pem".into()).expect("fail to encrypt");
+    fn test_encrypt() {
+        env_logger::init();
 
-        decrypt_file(
-            midi_file + "x".into(),
-            "test_private.key.pem".into(),
-            "30d9690cc085429a1d0a3ae787932bf1518a1798".into(),
-            "result".into(),
-        )
-        .expect("fail to encrypt");
+        let midi_file: String = "lalala1.mid".into();
+        encrypt_decrypt(midi_file);
+        encrypt_decrypt("113-BennyHill.mid".into());
     }
+
+   
 }
